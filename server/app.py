c44730ad87108a96f1b39c0ac7adf72b240e87cd
Nick Possokhov <NPOSSOKH@travelers.com>
2024-02-07 14:00:48 -0500

    fixed prediction response

--- server/app.py
+++ server/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import pickle
 
 app = Flask(__name__)
-
-model = None
 
 unit_types = ['unknown', 'at-st', 'stormtrooper', 'x-wing', 'tie_silencer', 'tie_fighter', 
         'at-at', 'resistance_soldier']
@@ -38,16 +36,24 @@ def predict():
     df["unit_type"] = pd.Categorical({unit_type}, categories=unit_types)
     df["homeworld"] = pd.Categorical({homeworld}, categories=homeworlds)
     df = pd.get_dummies(df)
-    df = df[model.feature_names_in_]
+    df = df[feature_names]
 
     is_resistance = model.predict(df)[0]
 
-    if is_resistance:
-        return "Resistance"
-    else:
-        return "Empire"
+    res = {
+        "is_resistance": str(is_resistance),
+        "accuracy_score": 0.625,
+        "feature_importances": feature_importances
+    }
 
+    return jsonify(res)
+    
 if __name__ == "__main__":
     model = pickle.load(open('trained_model.pkl', 'rb'))
+    
+    feature_names = model.feature_names_in_.tolist()
+    importances = model.feature_importances_.tolist()
+    feature_importances = {feature_names[i]: importances[i] for i in range(len(feature_names))}
+
     app.run(port=5000, debug=True)
 
